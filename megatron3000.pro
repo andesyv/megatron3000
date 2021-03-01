d202a528d4915e96fb8f981137d88a7ddc748eae
Anders Syvertsen <andesyv@gmail.com>
2021-03-01 14:15:06 +0100

    Basic Qt project with QMake setup

--- megatron3000.pro
+++ megatron3000.pro
@@ -1,6 +1,13 @@
-QT       += core
+QT       += core gui widgets
 
 CONFIG += c++17
 
 SOURCES += \
-    src/main.cpp
+    src/main.cpp \
+    src/mainwindow.cpp
+
+HEADERS += \
+    src/mainwindow.h
+
+FORMS += \
+    src/mainwindow.ui
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
+#include "mainwindow.h"
 
-int main() {
-    std::cout << "Hello world!" << std::endl;
-    return 0;
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
