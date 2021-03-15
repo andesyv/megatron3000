70814609294b3656b1d37cdb61675474a587f623
Anders Syvertsen <andesyv@gmail.com>
2021-03-15 22:45:28 +0100

    Texture2D setup

--- src/volume.cpp
+++ src/volume.cpp
@@ -21,14 +21,14 @@ bool Volume::loadData(const QString &fileName)
     QDataStream stream(&file);
     stream.setByteOrder(QDataStream::LittleEndian);
 
-    unsigned short width = 0, height = 0, depth = 0;
-    stream >> width >> height >> depth;
+    // unsigned short width = 0, height = 0, depth = 0;
+    stream >> m_width >> m_height >> m_depth;
 
-    qDebug() << "Width:" << width;
-    qDebug() << "Height:" << height;
-    qDebug() << "Depth:" << depth;
+    qDebug() << "Width:" << m_width;
+    qDebug() << "Height:" << m_height;
+    qDebug() << "Depth:" << m_depth;
 
-    int volumeSize = int(width)*int(height)*int(depth);
+    int volumeSize = int(m_width)*int(m_height)*int(m_depth);
     m_volumeData.resize(volumeSize);
 
     if (stream.readRawData(reinterpret_cast<char*>(m_volumeData.data()),volumeSize*sizeof (unsigned short)) != volumeSize*sizeof (unsigned short))
@@ -36,6 +36,47 @@ bool Volume::loadData(const QString &fileName)
         return false;
     }
 
+    generateTexture();
 
     return true;
+}
+
+void Volume::bind(GLuint binding) {
+    if (!m_texInitiated)
+        return;
+
+    m_binding = binding;
+    glActiveTexture(GL_TEXTURE0 + binding);
+    glBindTexture(GL_TEXTURE_2D, m_texBuffer);
+}
+
+void Volume::unbind() {
+    if (!m_texInitiated || !m_binding)
+        return;
+    
+    glActiveTexture(GL_TEXTURE0 + *m_binding);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    m_binding = std::nullopt;
+}
+
+void Volume::generateTexture() {
+    initializeOpenGLFunctions();
+
+    glGenTextures(1, &m_texBuffer);
+    glBindTexture(GL_TEXTURE_2D, m_texBuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_width, m_height, 0, GL_RED, GL_UNSIGNED_SHORT, m_volumeData.data());
+    // Min and mag filter: bilinear scaling
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // Wrap mode: just clamp to edge since edge is 0
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+    m_texInitiated = true;
+}
+
+Volume::~Volume() {
+    if (m_texInitiated)
+        glDeleteTextures(1, &m_texBuffer);
 }
--- src/volume.h
+++ src/volume.h
@@ -1,8 +1,10 @@
 #ifndef VOLUME_H
 #define VOLUME_H
 #include <QObject>
+#include <QOpenGLFunctions_4_3_Core>
+#include <optional>
 
-class Volume : public QObject
+class Volume : public QObject, protected QOpenGLFunctions_4_3_Core
 {
     Q_OBJECT
 public:
@@ -10,8 +12,21 @@ public:
 
     bool loadData(const QString &fileName);
 
+    void bind(GLuint binding = 0);
+    void unbind();
+
 private:
+    unsigned short m_width{0}, m_height{0}, m_depth{0};
+
     QVector<unsigned short> m_volumeData;
+    GLuint m_texBuffer;
+    bool m_texInitiated{false};
+    std::optional<GLuint> m_binding{std::nullopt};
+
+    void generateTexture();
+
+public:
+    ~Volume();
 };
 
 #endif // VOLUME_H
